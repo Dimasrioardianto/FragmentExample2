788be7b6030ad31bed5b7e5ba37e448675297d67
DimasRioArdianto <daffaprasetyo2017@gmail.com>
2024-10-28 15:20:37 +0700

    Update Commit 1

---
+++ app/src/main/java/id/ac/polbeng/ardianto/fragmentexample2/BookDescription.kt
@@ -0,0 +1,5 @@
+package id.ac.polbeng.ardianto.fragmentexample2
+
+class BookDescription {
+
+}
---
+++ app/src/main/java/id/ac/polbeng/ardianto/fragmentexample2/Coordinator.kt
@@ -0,0 +1,5 @@
+package id.ac.polbeng.ardianto.fragmentexample2
+
+interface Coordinator {
+    fun onBookChanged(index:Int)
+}
